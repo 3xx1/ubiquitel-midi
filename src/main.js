1d7c6ff6c2f2dcf27bf0aeb350715b3247600416
Hidekazu Saegusa <hidekazu.saegusa@frogdesign.com>
2018-02-03 23:46:47 -0800

    introducing gulp and express server

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
 var electron = require('electron');
+var express = require('express');
+var path = require('path');
+
+// express server config
+var server = express();
+server.use(express.static('public'));
+server.listen(3000, '127.0.0.1');
+
+// electron app config
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
-
 var mainWindow = null;
 
 app.on('window-all-closed', function() {
@@ -12,11 +20,9 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+  mainWindow.loadURL('http://127.0.01:3000');
 });
